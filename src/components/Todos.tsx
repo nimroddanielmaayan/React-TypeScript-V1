d5f82f116253b2bb75000a1c6a378316c24fe6c1
nimroddanielmaayan <nimrodmayan@gmail.com>
2023-05-12 23:48:33 +0300

    Daily progress update

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
 import TodoItem from './TodoItem';
-import Todo from '../models/todo';
+import { TodosContext } from '../store/todos-context';
 import classes from './Todos.module.css';
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC = () => {
+  const todosCtx = React.useContext(TodosContext);
+
   return (
     <ul className={classes.todos}>
-      {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text} />
+      {todosCtx.items.map((item) => (
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
